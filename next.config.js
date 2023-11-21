20f0e5997dd039f27c31e077f6cf360768c65654
WitherBlock <witherblock@protonmail.com>
2023-11-21 04:57:14 +0400

    feat: add redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   i18n,
+  async redirects() {
+    return [
+      {
+        source: '/frontier',
+        destination: 'https://galxe.com/Dopex/campaign/GCfLytU9Qf',
+        permanent: false,
+      },
+    ];
+  },
 };
